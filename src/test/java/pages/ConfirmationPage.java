e41b25e3195f646136201957d0ffb4ec387449d5
Jamnesh Maniya <jammymaniya10@gmail.com>
2026-03-15 12:11:23 +0530

    fix: add wait in ConfirmationPage msg method

--- src/test/java/pages/ConfirmationPage.java
+++ src/test/java/pages/ConfirmationPage.java
@@ -1,18 +1,28 @@
 package pages;
 
+import java.time.Duration;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class ConfirmationPage {
-	WebDriver driver;
-	
-	public ConfirmationPage(WebDriver driver) {
-		this.driver = driver;
-	}
-	
-	By confirmationMsg = By.tagName("h1");
-	
-	public String msg() {
-		return driver.findElement(confirmationMsg).getText();
-	}
+
+    WebDriver driver;
+
+    public ConfirmationPage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    By confirmationMsg = By.tagName("h1");
+
+    public String msg() {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        WebElement heading = wait.until(
+            ExpectedConditions.visibilityOfElementLocated(confirmationMsg)
+        );
+        return heading.getText();
+    }
 }
